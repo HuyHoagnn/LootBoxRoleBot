2d9601e5b1182a7810c362be8da7f7b07fe35139
HuyHoagnn <hhoang91105@gmail.com>
2026-07-24 18:04:17 +0700

    feat: đổi tên bộ role mới

--- utils/roles.js
+++ utils/roles.js
@@ -2,61 +2,61 @@ module.exports = [
 
     {
         id: "1530141560958746664",
-        name: "🐣 Gà Con Mới Nở",
+        name: "👤 Dân thường",
         chance: 20
     },
 
     {
         id: "1530141753171251250",
-        name: "🐸 AFK Chuyên Nghiệp",
+        name: "⛓️ Nô lệ",
         chance: 18
     },
 
     {
         id: "1530141838323875861",
-        name: "🥔 Củ Khoai Online",
+        name: "🎌 🎌 Wibu",
         chance: 15
     },
 
     {
         id: "1530141911166222336",
-        name: "🦆 Treo Cho Có",
+        name: "👗 Hầu gái",
         chance: 12
     },
 
     {
         id: "1530142007593406524",
-        name: "🐷 Voice Thủ",
+        name: "👑 Có thai cứ khai anh là bố",
         chance: 10
     },
 
     {
         id: "1530142066787487804",
-        name: "🪨 Không Mic Nhưng Có Mặt",
+        name: "💜 Mõm",
         chance: 9
     },
 
     {
         id: "1530142146483720212",
-        name: "😴 Ngủ Trong Voice",
+        name: "🔥 Cháy go",
         chance: 7
     },
 
     {
         id: "1530142236174581912",
-        name: "👻 Ma Phòng Voice",
+        name: "🕴️ Cậu chủ của Hân",
         chance: 5
     },
 
     {
         id: "1530142297486917673",
-        name: "🐉 Thần Treo Máy",
+        name: "🐉 Đại nhân",
         chance: 3
     },
 
     {
         id: "1530142371143090181",
-        name: "✨ Con Cưng RNG",
+        name: "✨👑 King",
         chance: 1
     }
 
